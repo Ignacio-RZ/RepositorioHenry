b52ccc5a3f2da22ebfdae7ea038250ee473195ef
ignacioRZ <ignale.ruiz@gmail.com>
2023-11-10 22:19:54 -0300

    Ahora agregamos bucles con una explicacion

---
+++ Bucle.js
@@ -0,0 +1,21 @@
+var suma = 0;
+//suma = suma +1;
+//suma = suma +2;
+//suma = suma +3;
+//suma = suma +4;
+
+//console.log (suma)
+
+//for (var i=0 ; i<5 ; i++){
+//    suma = suma + i;
+//    console.log("Variable de interacion: ", i);
+//}
+//console.log("Variable suma: ", suma)
+// Se utiliza cuando sabemos la cantida de pasos que queremos ejecutar.
+
+//while (suma < 3){
+//    suma = suma + 1;
+//    console.log(suma);
+//}
+// Se utiliza cuando no sabemos la cantidad de pasos que necesitaremos para finalizar la ejecucion
+// Se repetira hasta que la condicion o variable (si no entiendo mal) que establecimos se cumpla, en este caso es "3" cuando lleag a 3 finaliza la ejecucion.
